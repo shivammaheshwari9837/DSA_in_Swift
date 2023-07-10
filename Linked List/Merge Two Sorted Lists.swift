4555713511d5a048965f5bbe2d35112b87bc09cc
Shivam Maheshwari <56878460+shivammaheshwari9837@users.noreply.github.com>
2023-07-11 00:07:15 +0530

    Create Merge Two Sorted Lists.swift

---
+++ Linked List/Merge Two Sorted Lists.swift
@@ -0,0 +1,27 @@
+/*
+Problem Link : https://leetcode.com/problems/merge-two-sorted-lists/
+*/
+
+class Solution {
+    func mergeTwoLists(_ list1: ListNode?, _ list2: ListNode?) -> ListNode? {
+        var headNode = ListNode()
+        var tail = headNode
+        var list1 = list1
+        var list2 = list2
+
+        while list1 != nil && list2 != nil {
+            if list1!.val <= list2!.val {
+                tail.next = list1
+                list1 = list1?.next
+            } else {
+                tail.next = list2
+                list2 = list2?.next
+            }
+            tail = tail.next!
+        }
+
+        tail.next = (list1 == nil) ? list2 : list1
+
+        return headNode.next
+    }
+}
